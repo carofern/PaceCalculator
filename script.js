c6e6f4b4afc295bd382a0cab588e97bf31ccc5ef
Caro <caro@192.168.1.6>
2022-03-28 23:40:46 -0300

    Libreria

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Calcular ritmo: divido tiempo total / total de km corridos
-const storeElements = localStorage.getItem('elements')
+let storeElements = localStorage.getItem('elements')
 let array = [];
 
 //Spread nose si aplique bien el Spread
@@ -8,27 +8,16 @@ console.log(arraySpread);
 
 console.log(localStorage.getItem('elements'));
 
-let getData = function () {
-    const tiempoTotal = document.getElementById('ttHour').value;
-    const distancia = document.getElementById('dist').value;
-
-    //Operador ternario
-    (distancia.trim()) && (tiempoTotal.trim()) ? array.push(tiempoTotal) : alert('Ingresa un dato')
-    
-    if (distancia.trim() && tiempoTotal.trim()) {
-        array.push(tiempoTotal);
-        array.push(distancia);
-        document.getElementById('dist').value = '';
-        document.getElementById('ttHour').value = '';
-        mostrarUltimos();
-        localStorage.setItem('elements', JSON.stringify(array))
-    } else {
-        alert('Ingresa un dato')
-    }
-}
-
-
-
+const buttonReset = document.getElementById('resetButton')
+buttonReset.addEventListener('click', () => {
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Reinicio correcto',
+        showConfirmButton: false,
+        timer: 1500
+    })
+});
 
 
 function mostrarUltimos() {
@@ -49,19 +38,32 @@ function mostrarUltimos() {
         resultado[indice] = elemento / dividor;
     });
 
-    console.log(`Corri a ${resultado[0]} min por km`);
+    let resultUno = console.log(`Corri a ${resultado[0]} min por km`);
 }
 
 //Eventos Click
-const button = document.querySelector("#runButton")
+let getData = function () {
+    const tiempoTotal = document.getElementById('ttHour').value;
+    const distancia = document.getElementById('dist').value;
+
+    //Operador ternario
+    //(distancia.trim()) && (tiempoTotal.trim()) ? array.push(tiempoTotal) : alert('Ingresa un dato')
+
+    if (distancia.trim() && tiempoTotal.trim()) {
+        array.push(tiempoTotal);
+        array.push(distancia);
+        document.getElementById('dist').value = '';
+        document.getElementById('ttHour').value = '';
+        mostrarUltimos();
+       // getElementById('result').innerHTML = resultUno
+        localStorage.setItem('elements', JSON.stringify(array))
+    } else {
+        alert('Ingresa un dato')
+    }
+}
+
+//Eventos Click
+const button = document.getElementById('runButton')
 button.addEventListener('click', getData);
-
-//En construccion
-// const buttonReset = document.querySelector("#resetButton");
-// buttonReset.addEventListener('click', clearAll);
-
-// function clearAll() {
-    
-// }
 
 
